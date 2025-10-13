d2eea68eb0d4b1f124c51a5015aca21399a69cc2
jahnhtl <philipp.jahn@htlwien10.at>
2025-10-13 16:44:25 +0200

    read count from serial

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,17 @@ void setup() {
 void loop() {
   int count;
 
-  // Flush any existing data in the serial buffer
-  while (Serial.available()) {
-    Serial.read();
-  }
-
   Serial.print("\nEnter countdown start number: ");
 
-  while (!Serial.available()) {
-    // wait for user input
-  }
+  count = Serial.read();
 
-  count = Serial.parseInt();
-    
   for(int i = count; i > 0; i--) {
     Serial.println(i);
     delay(1000);
   }
 
   Serial.println("Ready for Takeoff!");
+
   delay(2000);
 }
 
